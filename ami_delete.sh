e6f682e1208230a0340ceb215170ea4df3f4ea6e
gonzalez <edds718@gmail.com>
2015-04-17 11:03:29 -0400

    formatting

--- ami_delete.sh
+++ ami_delete.sh
@@ -1,23 +1,22 @@
 #!/bin/sh
 # ------------------------------------------------------------------
-# [github.com/gonzalez]
-#          Title
-#          ami_delete.sh
+# [http://github.com/gonzalez]
+#  Name:
+#   ami_delete.sh
 #
-#          Description
-#          This script finds the ami's snapshot based on ami id,
-#          deregisters the ami and removes the snapshot
+#  Description:
+#   This script finds the ami's snapshot based on ami id,
+#   deregisters the ami and removes the snapshot
 #
-#           Usage
-#           ./ami_delete.sh ami-xxxx
+#  Usage:
+#   ./ami_delete.sh ami-xxxx
 #
-#
-#
-# Dependency:
-#     https://github.com/aws/aws-cli installed locally.
+#  Dependency:
+#   https://github.com/aws/aws-cli installed locally.
 # ------------------------------------------------------------------
 
 AMI=$1
+#todo - replace with regex
 SNAPSHOTID=`aws ec2 describe-images --image-ids $AMI |grep SnapshotId|cut -d  "\"" -f4`
 
 echo "Deregistering $AMI . . . "
