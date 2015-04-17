7ed1168b0253560ef36013892271d074e3565e46
gonzalez <edds718@gmail.com>
2015-04-17 10:53:57 -0400

    a place to store packer goods

---
+++ ami_delete.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+# ------------------------------------------------------------------
+# [github.com/gonzalez]
+#          Title
+#          ami_delete.sh
+#
+#          Description
+#          This script finds the ami's snapshot based on ami id,
+#          deregisters the ami and removes the snapshot
+#
+#           Usage
+#           ./ami_delete.sh ami-xxxx
+#
+#
+#
+# Dependency:
+#     https://github.com/aws/aws-cli installed locally.
+# ------------------------------------------------------------------
+
+AMI=$1
+SNAPSHOTID=`aws ec2 describe-images --image-ids $AMI |grep SnapshotId|cut -d  "\"" -f4`
+
+echo "Deregistering $AMI . . . "
+`aws ec2 deregister-image --image-id $AMI`
+
+sleep 15
+
+echo "Deleting Snapshot $SNAPSHOTID associated with $AMI"
+`aws ec2 delete-snapshot --snapshot-id $SNAPSHOTID`
